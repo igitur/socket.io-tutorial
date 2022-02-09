1c0a3600585949db203d8c4ec07a258bf2ed04b8
Erwin Acuario Jr <erwinprograms@gmail.com>
2022-02-09 14:39:57 -0800

    Add messaging functionality

--- client/script.js
+++ client/script.js
@@ -1,7 +1,19 @@
+import { io } from 'socket.io-client';
+
 const joinRoomButton = document.getElementById('room-button');
 const messageInput = document.getElementById('message-input');
 const roomInput = document.getElementById('room-input');
 const form = document.getElementById('form');
+
+const socket = io('http://localhost:3000')
+socket.on( 'connect', () => {
+    displayMessage(`You connected with id: ${socket.id}`);
+    socket.emit('custom-event', 10, 'hi', {a: 'a'})
+})
+
+socket.on('recieve-message', message => {
+    displayMessage(message);
+})
 
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -10,6 +22,7 @@ form.addEventListener('submit', e => {
 
     if(message === "") return
     displayMessage(message)
+    socket.emit('send-message', message);
 
     messageInput.value = "";
 })
