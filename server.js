dbc703c18e9492b66de25643cea9295c96cad625
Erwin Acuario Jr <erwinprograms@gmail.com>
2022-02-09 14:52:54 -0800

    Add rooms and private messaging

--- server.js
+++ server.js
@@ -6,8 +6,18 @@ const io = require('socket.io')(3000, {
 
 io.on('connection', socket => {
     console.log(socket.id)
-    socket.on('send-message', (message) => {
+    socket.on('send-message', (message, room) => {
         // io.emit('recieve-message', message); //server emmits message to every connection
-        socket.broadcast.emit('recieve-message', message);  //socket broadcasts on every socket that isn't itself
+        if (room === ''){
+            socket.broadcast.emit('recieve-message', message);  //socket broadcasts on every socket that isn't itself
+        } else {
+            //private message based on room id
+            socket.to(room).emit('recieve-message', message);
+        }
+    })
+
+    socket.on('join-room', (room, callback) => {
+        socket.join(room);
+        callback(`Joined ${room}`);
     })
 })
